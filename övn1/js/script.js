0897d3cf350294c7b515d9d40a628444996af2e7
Morso <69962221+Morso33@users.noreply.github.com>
2021-01-27 12:39:31 +0200

    Initial commit

---
+++ övn1/js/script.js
@@ -0,0 +1,19 @@
+const hej = "hej på dig"
+const heltal = (3)
+const decimaltal = (3.3)
+const divide = (1/2)
+const myclass = "pu20h"
+const myschool = "Prakticum"
+var calc = Math.ceil(3*4,765);
+
+
+
+console.log(calc)
+console.log(hej)
+console.log(heltal)
+console.log(decimaltal)
+console.log(divide)
+
+
+
+
